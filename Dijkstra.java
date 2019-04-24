61c79a7cec498ab0373897ee8a177e6c592ae8f6
s1250102-Minamisawa <s1250102@u-aizu.ac.jp>
2019-04-24 11:57:05 +0900

    s1250102 modified Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -3,12 +3,22 @@ class Dijkstra{ //referenced to p305 algorithms
     private int w=0;
     private int g=1;
     private int b=2;
+    private int color[];
+    private double d[];
+    private int p[];
+    private int size;
+    private int psize;
+    private int st;
+    private int ed;
     Dijkstra(){
     }
-    double nTon(double[][]n,int st,int ed,int size){
-	int[]color=new int[size+1];
-	double []d=new double[size+1];
-	int[]p=new int[size+1];
+    double nTon(double[][]n,int st,int ed,int size,int psize){
+	color=new int[size+1];
+	d=new double[size+1];
+	p=new int[size+1];
+	this.ed=ed;
+	this.st=st;
+	this.psize=psize;
 	double hz;
 	int u,v;
 	double mincost;
@@ -43,10 +53,43 @@ class Dijkstra{ //referenced to p305 algorithms
 			p[v]=u;
 			color[v]=g;
 		    }
+		    //		    if(d[v]=d[u]+n[u][v]){}
 		}
 	    }
 	}
 	if(d[ed]==INF)return -1.0;
 	return d[ed];
+    }
+    public String rep_NumtoS(int d,boolean bla){
+	String s1,s2="";
+	if(bla)s2=" ";
+	if(psize<d){
+	    s1=Integer.toString(d-psize);
+	    s2=s2+"C";
+	}else{
+	    s1=Integer.toString(d);
+	}
+	return s2+s1;
+    }
+    public String Route(int ed){
+	String c="";
+	int i,po;
+	po=p[ed];
+	if(po!=-1){c=rep_NumtoS(ed,true)+c;
+         }else {
+	  c=rep_NumtoS(ed,false)+c;
+	}
+	while(po!=-1){
+	 if(p[po]==-1){
+	     c=rep_NumtoS(po,false)+c;
+	 }else{
+	     c=rep_NumtoS(po,true)+c;
+	 }
+	    po=p[po];
+	}
+	return c;
+    }
+    public String retRoute(){
+	return Route(this.ed);
     }
 }
